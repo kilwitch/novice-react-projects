9075f345735fec6649b5817c7607f567f6954254
wahid <barbhuiyawahid08@gmail.com>
2026-02-25 00:59:21 +0530

    used react router

---
+++ 07landing/src/components/Home/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import heroImage from '../../assets/heroImage.svg'
+import photo2 from '../../assets/photo2.svg'
+
+
+function Home() {
+  return (
+     <div className="mx-auto w-full max-w-7xl">
+            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+                <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+                    <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+                        <h2 className="text-4xl font-bold sm:text-5xl">
+                            Download Now
+                            <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
+                        </h2>
+
+                        <Link
+                            className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
+                            to="/"
+                        >
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-2">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+  </svg>
+  Download now  
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+                    <img className="w-96" src={heroImage} alt="image1" />
+                </div>
+            </aside>
+
+            <div className="grid  place-items-center sm:mt-20">
+                <img className="sm:w-96 w-48" src={photo2} alt="image2" />
+            </div>
+
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
+        </div>
+  )
+}
+
+export default Home
